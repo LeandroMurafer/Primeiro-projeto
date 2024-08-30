f5e90380199a5827dde968c6103c15abe13c2125
LeandroMurafer <Leandromuraferr@gmail.com>
2024-08-29 22:24:43 -0300

    Add files via upload

---
+++ JDK test/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main (String[] args) {
+           System.out.println("Ferver agua");
+           System.out.println("Colocar po no filtro");
+           System.out.println("Colocar copo abaixo do filtro");
+           System.out.println("Colocar agua quente no filtro");
+           System.out.println("Esperar cafe coar");
+           System.out.println("Verificar se o café esta no copo");
+           System.out.println("Servir o cafe coado pronto para o consumo");
+    
+    }
+
+}
